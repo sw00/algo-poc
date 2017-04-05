97a174e5f95add5252edd7bbd86d05c683fcd7f4
sw00 <sett.wai@gmail.com>
2017-04-05 11:01:56 +0200

    [farzana, sett] add quickfix package and example app

---
+++ example.py
@@ -0,0 +1,16 @@
+import quickfix
+
+if len(sys.argv) < 2: return
+fileName = sys.argv[1]
+
+try:
+        settings = quickfix.SessionSettings(fileName)
+        application = quickfix.MyApplication()
+        storeFactory = quickfix.FileStoreFactory(settings)
+        logFactory = quickfix.FileLogFactory(settings)
+        acceptor = quickfix.SocketAcceptor(application, storeFactory, settings, logFactory)
+        acceptor.start()
+        # while condition == true: do something
+        acceptor.stop()
+except quickfix.ConfigError, e:
+        print e
