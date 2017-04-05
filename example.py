ff0341e5bc6bb6d55f50cb91dde5450b222bdd72
sw00 <sett.wai@gmail.com>
2017-04-05 14:05:08 +0200

    [farzana,sett] initial example initiator

--- example.py
+++ example.py
@@ -1,14 +1,26 @@
 import quickfix
+import sys
 
-if len(sys.argv) < 2: return
-fileName = sys.argv[1]
+class Application(quickfix.Application):
+        def onCreate(self, sessionID): return
+        def onLogon(self, sessionID): return
+        def onLogout(self, sessionID): return
+        def toAdmin(self, message, sessionID): return
+        def toApp(self, message, sessionID): return
+        def fromAdmin(self, message, sessionID): return
+        def fromApp(self, message, sessionID): return
+
+if len(sys.argv) < 2: 
+	exit(1)
+else:
+	fileName = sys.argv[1]
 
 try:
         settings = quickfix.SessionSettings(fileName)
-        application = quickfix.MyApplication()
+        application = Application()
         storeFactory = quickfix.FileStoreFactory(settings)
         logFactory = quickfix.FileLogFactory(settings)
-        acceptor = quickfix.SocketAcceptor(application, storeFactory, settings, logFactory)
+        acceptor = quickfix.SocketInitiator(application, storeFactory, settings, logFactory)
         acceptor.start()
         # while condition == true: do something
         acceptor.stop()
