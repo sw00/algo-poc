2589cc7a8774e93f53213de4a228926662a4c6fa
frassultw <frassul@thoughtworks.com>
2017-04-06 07:36:13 -0700

    [farzana, sett] progress commit

--- QuickFixPOC/Simulator/Application.cs
+++ QuickFixPOC/Simulator/Application.cs
@@ -5,16 +5,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using QuickFix;
 
 namespace Simulator
 {
-    public class Application
+    public interface ISessionCreator
+    {
+        Session LookupSession(SessionID sessionId);
+    }
+
+    public class SessionCreator : ISessionCreator
+    {
+        public Session LookupSession(SessionID sessionId)
+        {
+            return Session.LookupSession(sessionId);
+        }
+    }
+
+    public class Application : QuickFix.IApplication
     {
         public List<string> messages;
+        private Session _session;
+        private ISessionCreator _createSession;
         
-        public Application(string filename)
+        public Application(string filename, ISessionCreator sessionCreator)
         {
             messages = File.ReadLines(filename).ToList();
+            _createSession = sessionCreator;
+        }
+
+        public Application(string filename, Session fakeSession)
+        {
+            messages = File.ReadLines(filename).ToList();
+        }
+
+        public void FromAdmin(Message message, SessionID sessionID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FromApp(Message message, SessionID sessionID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCreate(SessionID sessionID)
+        {
+            _session = _createSession.LookupSession(sessionID);
+        }
+
+        public void OnLogon(SessionID sessionID)
+        {
+            
+        }
+
+        public void OnLogout(SessionID sessionID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToAdmin(Message message, SessionID sessionID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToApp(Message message, SessionID sessionId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- QuickFixPOC/SimulatorTests/SimulatorTests.cs
+++ QuickFixPOC/SimulatorTests/SimulatorTests.cs
@@ -1,4 +1,6 @@
-﻿using Simulator;
+﻿using Moq;
+using QuickFix;
+using Simulator;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +10,48 @@ using Xunit;
 
 namespace SimulatorTests
 {
+    public class MockSessionCreator : ISessionCreator
+    {
+        Mock<Session> _mockSession;
+
+        public Mock<Session> getMockSession()
+        {
+            return _mockSession;
+        }
+
+        public Session LookupSession(SessionID sessionId)
+        {
+            _mockSession = new Mock<Session>();
+          
+            return _mockSession.Object;
+        }
+    }
+
+
     public class SimulatorTests
     {
         [Fact]
         public void Should_LoadMessagesFromFilePath()
         {
             string path = "Data/03.JSE_Indices_recv_log.txt";
-            var app = new Application(path);
+            var app = new Simulator.Application(path, new MockSessionCreator());
 
             Assert.True(app.messages.Count > 0);
+        }
+
+        [Fact]
+        public void Should_SendMessages()
+        {
+            string path = "Data/03.JSE_Indices_recv_log.txt";
+            var mockSessionCreator = new MockSessionCreator();
+
+            var app = new Simulator.Application(path, mockSessionCreator);
+
+            app.OnLogon(new SessionID("", "", ""));
+
+            mockSessionCreator.getMockSession().Verify(s => s.Send(It.IsAny<Message>()), Times.Exactly(1));         
+
+
         }
     }
 }
